ac9e7eaf8575697021a194af3d64c8f322a0b399
Ignácio Amstalden <77023544+Celti4@users.noreply.github.com>
2021-03-15 20:11:58 -0300

    Add files via upload
    Without trash,

--- NextPurchaseDateMarkov.py
+++ NextPurchaseDateMarkov.py
@@ -11,14 +11,6 @@ def MarkovNextPurchaseDate(lastDay, dataFrame):
 
 
 
-#def MarkovNextPurchaseDate(probVector): 
-#    day = np.random.multinomial(1, probVector, size=1)
-#    dayOfWeek = np.array(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
-#    indexDay = np.where(day == 1)[1]
-#    dayPredict = dayOfWeek[indexDay][0]
-#    return(dayPredict)
-
-
 def randomDay(): 
     probVector = np.array([1/7,1/7,1/7,1/7,1/7,1/7,1/7])
     day = np.random.multinomial(1, probVector, size=1)
